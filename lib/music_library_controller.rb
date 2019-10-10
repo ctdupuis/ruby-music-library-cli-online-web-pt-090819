2371cf4e3dd96b24e59bd6bc2ceb9e0751a9229d
Cody Dupuis <ctdupuis7@gmail.com>
2019-10-10 19:49:33 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -30,11 +30,11 @@ class MusicLibraryController
       when "list genres"
         list_genres
       when "list artist"
-        
+        list_songs_by_artist
       when "list genre"
-      
+        list_songs_by_genre
       when "play song"
-        
+        play_song
       end
     end
   end
