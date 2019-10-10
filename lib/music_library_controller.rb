d729ed0477b3a494dc308b31a7859bef764a19d1
Cody Dupuis <ctdupuis7@gmail.com>
2019-10-10 19:28:19 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -46,7 +46,10 @@ class MusicLibraryController
     puts "Please enter the name of an artist:"
     input = gets.strip
     
-    
+    if artist = Artist.find_by_name(input)
+     puts artist.songs
+      
+    end
     # binding.pry
   end
 end
