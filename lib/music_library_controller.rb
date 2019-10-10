86c18a8e5d3b5e389389d74c2acffbaf5d61d8cc
Cody Dupuis <ctdupuis7@gmail.com>
2019-10-10 19:15:55 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -32,7 +32,7 @@ class MusicLibraryController
   
    def list_artists
     Artist.all.sort_by(&:name).each.with_index(1) do |a, i|
-      puts "#{i}. #{a.artist.name} - #{a.name} - #{a.genre.name}"
+      puts "#{i}. #{a.name}"
     end
   end
 end
