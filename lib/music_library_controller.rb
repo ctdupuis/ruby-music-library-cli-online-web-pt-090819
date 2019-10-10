440acc8afa6e27941eb7981046addb72f5351b82
Cody Dupuis <ctdupuis7@gmail.com>
2019-10-10 19:39:33 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -68,6 +68,8 @@ class MusicLibraryController
     puts "Which song number would you like to play?"
     
     input = gets.strip.to_i
-    if input
+    if (1..Song.all.length).include?(input)
+      
+    end
   end
 end
